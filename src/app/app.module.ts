783f238ca21dea01cc4c356afbf1f4b11802afb0
JBarr96 <jbarr@smu.edu>
2017-10-29 21:38:53 -0500

    Basic login pages w/o routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ 
+    BrowserModule,
+    LoginModule
+  ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/login/login-new/login-new.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-new',
+  templateUrl: './login-new.component.html',
+  styleUrls: ['./login-new.component.css']
+})
+export class loginNewComponent {
+  constructor() {}
+}
