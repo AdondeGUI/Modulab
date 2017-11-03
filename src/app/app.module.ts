164bb770f76e31a7c2855cf463bca3e4427d75ed
cogden14 <cogden14@gmail.com>
2017-11-03 10:37:49 -0500

    Add LabModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LabModulesModule } from './LabModules'
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule,
+  LabModulesModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
