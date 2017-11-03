03f62bc3d9483203c00aa49eea2816697bc54e97
JBarr96 <jbarr@smu.edu>
2017-11-03 10:25:26 -0500

    Migrated to LabModules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LoginModule } from './login';
+import { LoginModule } from './LabModules/login';
 
 @NgModule({
   imports: [ 
