e154fda538c56edb517edc4de9f965e06fc9a482
cogden14 <cogden14@gmail.com>
2017-11-03 12:05:07 -0500

    Table sizing complete

--- src/app/LabModules/table-generator/table-generator.component.ts
+++ src/app/LabModules/table-generator/table-generator.component.ts
@@ -15,8 +15,32 @@ export class tableGeneratorComponent {
   }
 
   private adjustTableSize() {
-    var shit = {'one':this.rowNum,'two':this.colNum}
-    this.rows.push(shit);
+    console.log('Cols: ' + this.cols.length);
+    console.log('Rows: ' + this.rows.length);
+    console.log('ColNum: ' + this.colNum);
+    console.log('RowNum: ' + this.rowNum);
+
+  //  while(this.colNum != this.cols.length && this.rowNum != this.rows.length)
+  //  {
+      var col = {'one':this.colNum}
+      if(this.colNum > this.cols.length)
+        this.cols.push(col);
+      else if(this.colNum < this.cols.length)
+        this.cols.pop();
+      var shit = {'one':this.rowNum, 'two':this.colNum}
+      if(this.rowNum > this.rows.length)
+      {
+        console.log('Add Row');
+        console.log(shit);
+        this.rows.push(shit);
+      }
+      else if(this.rowNum < this.rows.length)
+      {
+        console.log('Pop Row');
+        this.rows.pop();
+      }
+//    }
+
     // var rowsVal = this.rowNum;
     // for(item in rows)
     //   delete this.rows();
