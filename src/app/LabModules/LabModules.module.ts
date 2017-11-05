220384cf481b37a1ddd4087eb6b5bbe688e5aaca
JBarr96 <jbarr@smu.edu>
2017-11-05 11:18:34 -0600

    ability to select and display multiple images from file

--- src/app/LabModules/LabModules.module.ts
+++ src/app/LabModules/LabModules.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { loginExistingComponent } from './login/login-existing/login-existing.component';
+import { loginNewComponent } from './login/login-new/login-new.component';
 import { tableGeneratorComponent } from './table-generator/table-generator.component';
 import { labDirectoryComponent } from './lab-directory/lab-directory.component';
+import { ImageModuleComponent } from './image-module/image-module.component';
 
 
 @NgModule({
@@ -11,12 +14,15 @@ import { labDirectoryComponent } from './lab-directory/lab-directory.component';
         FormsModule,
     ],
     declarations: [
+
         tableGeneratorComponent,
-        labDirectoryComponent
+        labDirectoryComponent,
+        ImageModuleComponent
     ],
     exports: [
         tableGeneratorComponent,
-        labDirectoryComponent
+        labDirectoryComponent,
+        ImageModuleComponent
     ]
 })
 export class LabModulesModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LoginModule } from './LabModules/login';
 import { LabModulesModule } from './LabModules'
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    LoginModule,
     LabModulesModule
   ],
   declarations: [ AppComponent ],
