e682ddf1e87f3b57bb165c26339f4862f4c2f3d2
cogden14 <cogden14@gmail.com>
2017-11-17 15:10:35 -0600

    Temporary app.component.ts for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,3 @@
-// import { Component, Input, OnInit } from '@angular/core';
-//
-// @Component({
-//   selector: 'app',
-//   templateUrl: 'app.component.html',
-//   styleUrls: [ 'app.component.css' ]
-// })
-//
-// export class AppComponent {
-//
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
