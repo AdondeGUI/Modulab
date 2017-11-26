fdf55196ab6a57f72330af5c5ad754031bb9c9f4
cogden14 <cogden14@gmail.com>
2017-11-26 17:21:41 -0600

    Table ngModel functionality

--- src/app/LabModules/table-generator/table-generator.component.ts
+++ src/app/LabModules/table-generator/table-generator.component.ts
@@ -9,12 +9,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TableGeneratorComponent {
   rows: any[] = [];
   cols: any[] = [];
+  table: any[][] = [];
   rowNum: number = 1;
   colNum: number = 1;
   constructor() {
   }
   @Input()
   public data : string;
+  // obj = JSON.parse(this.data);
 
   private adjustTableSize() {
     while(this.colNum != this.cols.length || this.rowNum != this.rows.length)
@@ -37,5 +39,22 @@ export class TableGeneratorComponent {
         this.rows.pop();
       }
     }
+     console.log(this.rows);
+
   }
+
+  // private saveTable() {
+  //   for (var i=0;i<this.rowNum; i++){
+  //     var row : any [];
+  //     for (var j=0;j<this.colNum;j++){
+  //       var x = document.getElementById("i j").name;
+  //       row.push(id=["i j"]);
+  //     }
+  //     this.table.push(row);
+  //   }
+  // }
+  //
+  // private changeValue(i : number, j: number) {
+  //   this.table[i][j] = cellValue;
+  // }
 }
